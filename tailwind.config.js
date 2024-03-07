937508d2f03d58458bb7eeccdf89e89eac4034bf
Matheus <matheusramosbi.13@gmail.com>
2024-03-07 13:47:47 -0300

    nav done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Complex site-specific column configuration
+        "body-grid": "1fr 120px minmax(300px, 800px) 1fr",
+      },
+    },
   },
   plugins: [],
 };
