2e256b042976fb6f333e064244ea7860568abf04
Matheus <matheusramosbi.13@gmail.com>
2024-03-11 11:05:10 -0300

    add responsiveness & text correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,26 @@ module.exports = {
       gridTemplateColumns: {
         // Complex site-specific column configuration
         "body-grid": "1fr 120px minmax(300px, 800px) 1fr",
+        "body-grid-sm": "1fr",
         "section-grid": "90px 300px 1fr",
+        "section-grid-sm": "100px 1fr",
       },
+    },
+    screens: {
+      sm: { max: "767px" },
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+
+      md: { min: "768px", max: "1023px" },
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+      lg: { min: "1024px", max: "1279px" },
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+      xl: { min: "1280px", max: "1535px" },
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+      "2xl": { min: "1536px" },
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [require("tailwindcss"), require("autoprefixer")],
