0a7ba8fcd7aa35d97e46bb33cfa8e7e303e30e2a
Matheus <matheusramosbi.13@gmail.com>
2024-03-26 11:48:32 -0300

    add `accordion list` on FAQ

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-//   tabContent[0].classList.remove("hidden");
+  //Adicionando as classes nos elementos para que seja exibido apenas o texto referente ao animal selecionado
+  if (tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+      tabContent.forEach((item) => {
+        item.classList.remove("block");
+        item.classList.add("hidden");
+      });
 
-if (tabMenu.length && tabContent.length) {
-  function activeTab(index) {
-    tabContent.forEach((item) => {
-      item.classList.remove("ativo");
-      item.classList.remove("block");
-      item.classList.add("hidden");
+      tabContent[index].classList.add("block");
+      tabContent[index].classList.add("animate-description");
+      tabContent[index].classList.remove("hidden");
+    }
+
+    tabMenu.forEach((itemMenu, indexMenu) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(indexMenu);
+      });
     });
-    tabContent[index].classList.add("ativo");
-    tabContent[index].classList.add("block");
-    tabContent[index].classList.add("animate-description");
-    tabContent[index].classList.remove("hidden");
+  }
+}
+
+function initAccordionList() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  accordionList[0].nextElementSibling.classList.toggle("block");
+  accordionList[0].nextElementSibling.classList.toggle("hidden");
+
+  function activeAccordion(event) {
+    event.currentTarget.nextElementSibling.classList.toggle("block");
+    event.currentTarget.nextElementSibling.classList.toggle("hidden");
+    event.currentTarget.nextElementSibling.classList.add("animate-slideShow");
+    event.currentTarget.classList.toggle("after:content-['⬎']");
+    event.currentTarget.classList.toggle("after:content-['⬏']");
   }
 
-  tabMenu.forEach((itemMenu, indexMenu) => {
-    itemMenu.addEventListener("click", () => {
-      activeTab(indexMenu);
-    });
+  accordionList.forEach((item) => {
+    item.addEventListener("click", activeAccordion);
   });
 }
+
+initTabNav();
+initAccordionList();
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,30 +12,35 @@ module.exports = {
       },
       animation: {
         description: "animal-description-enter 0.5s linear",
+        slideShow: "slideShow 0.5s",
       },
       keyframes: {
         "animal-description-enter": {
           "0%": { transform: "translateX(-1rem)", opacity: 0 },
           "100%": { transform: "translateX(0rem)", opacity: 100 },
         },
+        slideShow: {
+          "0%": { transform: "max-height: 0px", opacity: 0 },
+          "100%": { transform: "max-height: 200px", opacity: 100 },
+        },
+      },
+      screens: {
+        sm: { max: "767px" },
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+
+        md: { min: "768px", max: "1023px" },
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+        lg: { min: "1024px", max: "1279px" },
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+        xl: { min: "1280px", max: "1535px" },
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+        "2xl": { min: "1536px" },
+        // => @media (min-width: 1536px) { ... }
       },
     },
-    screens: {
-      sm: { max: "767px" },
-      // => @media (min-width: 640px and max-width: 767px) { ... }
-
-      md: { min: "768px", max: "1023px" },
-      // => @media (min-width: 768px and max-width: 1023px) { ... }
-
-      lg: { min: "1024px", max: "1279px" },
-      // => @media (min-width: 1024px and max-width: 1279px) { ... }
-
-      xl: { min: "1280px", max: "1535px" },
-      // => @media (min-width: 1280px and max-width: 1535px) { ... }
-
-      "2xl": { min: "1536px" },
-      // => @media (min-width: 1536px) { ... }
-    },
+    plugins: [require("tailwindcss"), require("autoprefixer")],
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
