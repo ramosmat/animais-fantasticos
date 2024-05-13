15fdcf65d56d70f353a93ab719e65a1fd85befdb
Matheus <matheusramosbi.13@gmail.com>
2024-05-13 09:57:37 -0300

    change `accordion` into class

--- js/modules/scroll-accordion.js
+++ js/modules/scroll-accordion.js
@@ -1,20 +1,25 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll(
-    '[data-anime="accordion"] dt'
-  );
-  const activeClass = "ativo";
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = "ativo";
+  }
 
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
 
-    function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
+  addAccordionEvent() {
+    this.accordionList.forEach((item) => {
+      item.addEventListener("click", () => this.toggleAccordion(item));
     });
+  }
+
+  //iniciar função
+  init() {
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0]); //Ativar primeiro item
+      this.addAccordionEvent();
+    }
   }
 }
